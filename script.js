850767303d0d7b80b513cbc653c4cb1dfe45a2bf
ilahratie <85931085+ilahratie@users.noreply.github.com>
2021-06-18 14:57:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFuction()
+{
+    var x = document.getElementsByClassName("zebra");
+
+for (var i = 0; i <x.length;i++)
+{
+    x[i].style.backgroundColor = "black";
+    x[i].style.color = "white";
+}
+
+}
